327c181de0338f4667b822055c64867507190e41
Arpan Sadhukhan <92740136+arpan-sadhukhan@users.noreply.github.com>
2022-12-28 13:39:01 +0530

    Create StarPrint2.java

---
+++ Assignment 27-12-2022/StarPrint2.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class StarPrint2 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;j<5;j++)
+			{
+				if(j<4-i)
+					System.out.print(" ");
+				else
+					System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
