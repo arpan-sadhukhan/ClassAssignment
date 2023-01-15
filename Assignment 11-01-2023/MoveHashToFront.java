18a91feb7d48cf3ddfdd1afb5ea2959fd7cd3075
Arpan Sadhukhan <92740136+arpan-sadhukhan@users.noreply.github.com>
2023-01-15 10:05:36 +0530

    Update MoveHashToFront.java

--- Assignment 11-01-2023/MoveHashToFront.java
+++ Assignment 11-01-2023/MoveHashToFront.java
@@ -1,3 +1,21 @@
+/*You have write a function that accepts, a string which length is “len”,
+the string has some “#”, in it you have to move all the hashes to the front of the string and return the whole string back and print it.
+
+char* moveHash(char str[],int n);
+
+example :-
+
+Sample Test Case
+
+Input:
+
+Move#Hash#to#Front
+
+Output:
+
+###MoveHashtoFront
+*/
+
 package string;
 
 import java.util.Scanner;
