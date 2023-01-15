cff806a4cbee071456d7d6d215dfcf34406b3d8a
Arpan Sadhukhan <92740136+arpan-sadhukhan@users.noreply.github.com>
2023-01-15 10:04:11 +0530

    Create MoveHashToFront.java

---
+++ Assignment 11-01-2023/MoveHashToFront.java
@@ -0,0 +1,35 @@
+package string;
+
+import java.util.Scanner;
+
+public class MoveHashToFront {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string: ");
+		String s=sc.nextLine();
+		StringBuilder strb=new StringBuilder(s);
+		int len=strb.length();
+		moveHash(strb, len);
+		
+
+	}
+	
+	public static void moveHash(StringBuilder str, int n)
+	{
+		int i,j;
+		char temp;
+		for(i=0;i<str.length();i++) {
+			if(str.charAt(i)=='#') {
+				for(j=i;j>0;j--) {
+					temp=str.charAt(j);
+					str.setCharAt(j, str.charAt(j-1));
+					str.setCharAt(j-1, temp);
+					
+				}
+			}
+		}		
+		System.out.println(str);
+	}
+
+}
