6ff0241244ddd615548edd6b9358086d90a18b7c
Arpan Sadhukhan <92740136+arpan-sadhukhan@users.noreply.github.com>
2023-02-05 22:27:45 +0530

    Create Employee.java

---
+++ Assignment 03-02-2023/Employee.java
@@ -0,0 +1,23 @@
+package Collection;
+
+public class Employee {
+int empid;
+String name;
+int salary;
+public Employee(int empid, String name, int salary) {
+	super();
+	this.empid = empid;
+	this.name = name;
+	this.salary = salary;
+}
+public int getEmpid() {
+	return empid;
+}
+public String getName() {
+	return name;
+}
+public int getSalary() {
+	return salary;
+}
+
+}
